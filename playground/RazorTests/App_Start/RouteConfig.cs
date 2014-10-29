234d76274295a57b8303738749bf8eb078ee8145
Marc Clifton <marc.clifton@gmail.com>
2014-10-28 20:01:57 -0400

    Added an inline editable grid test based on Mike Brind's MVC 3.0 inline editing control.  Fixed issues that came up with MVC 4.0 and also changed the code so that it doesn't use a JSON return string to update the display-only grid values, but does so immediately on the client-side.

---
+++ playground/RazorTests/App_Start/RouteConfig.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace RazorTests
+{
+	public class RouteConfig
+	{
+		public static void RegisterRoutes(RouteCollection routes)
+		{
+			routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+			routes.MapRoute(
+				name: "Default",
+				url: "{controller}/{action}/{id}",
+				defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+			);
+
+			routes.MapRoute(
+				name: "ViewBook",
+				url: "{controller}/{action}/{id}",
+				defaults: new { controller = "ViewBook", action = "Index", id = UrlParameter.Optional }
+			);
+
+			//routes.MapRoute(
+			//	name: "SaveBook",
+			//	url: "{controller}/{action}/{id}",
+			//	defaults: new { controller = "ViewBook", action = "SaveChanges", id = UrlParameter.Optional }
+			//);
+		}
+	}
+}
---
+++ playground/RazorTests/Controllers/ViewBookController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+
+using WebMatrix.Data;
+
+namespace RazorTests.Controllers
+{
+    public class ViewBookController : Controller
+    {
+        //
+        // GET: /EditBook/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+		[HttpPost]
+		public ActionResult SaveChanges()
+		{
+			var bookId = Request["BookId"];
+			var title = Request["Title"];
+			var authorId = Request["AuthorId"];
+			var categoryId = Request["CategoryId"];
+			var isbn = Request["ISBN"];
+			var hardback = Request["Hardback"];
+ 
+			var db = Database.Open("Books");
+
+			var sql = "UPDATE Books SET Title = @0, AuthorId = @1, CategoryId = @2, ISBN = @3, Hardback = @4 WHERE BookId = @5";
+			db.Execute(sql, title, authorId, categoryId, isbn, hardback, bookId);
+
+			// Not used, as the client updates itself.
+
+			/*
+			sql = @"SELECT b.Title, b.ISBN,  a.FirstName + ' ' + a.LastName AS AuthorName, c.Category, b.Hardback  
+            FROM Books b INNER JOIN Authors a ON b.AuthorId = a.AuthorId  
+            INNER JOIN Categories c ON b.CategoryId = c.CategoryId 
+            WHERE BookId = @0";
+			var result = db.QuerySingle(sql, bookId);
+			// Json.Write(result, Response.Output);
+			var jsonResult = System.Web.Helpers.Json.Encode(result);
+			 
+
+			return Json(jsonResult, JsonRequestBehavior.AllowGet);
+			*/
+
+			return new EmptyResult();
+		}
+    }
+}
